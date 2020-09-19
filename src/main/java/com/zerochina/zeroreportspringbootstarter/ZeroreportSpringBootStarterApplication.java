f01c544efafbba23da93d0a9c43c76c774a7c450
尹家权 <jiaquan.yin@huilianyi.com>
2020-09-19 23:00:48 +0800

    初始化插件

---
+++ src/main/java/com/zerochina/zeroreportspringbootstarter/ZeroreportSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.zerochina.zeroreportspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroreportSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroreportSpringBootStarterApplication.class, args);
+	}
+
+}
